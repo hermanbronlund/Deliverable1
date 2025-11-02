ed2f768cdcf05ffcb7fdb92aa36d962638e910fc
hermanbronlund <hebro6694@nmbu.no>
2025-11-02 21:41:48 +0100

    Helt snøfnugg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Modifisert koden slik at den virker, den virket ikke da jeg kun brukte det som kom fra canvas, slik det stod i instruksen. 
 Får riktig stjerne, og begynner derfor med å sette opp resten av koden, slik som oppgaven vil. 
+Endrer koden litt, slik at figuren ikke slutter ved kanten på vinduet, men heller holder seg midt på skjermen. Hele koden endres litt på. 
+Har støtt på endel problemer for å få sentrert figuren. Dette tar litt tid. 
+Koden er klar, slik at det kommer et helt snøfnugg opp. 
--- Snowflake.py
+++ Snowflake.py
@@ -1,24 +1,31 @@
 import turtleplotlib as tpl
 import matplotlib.pyplot as plt
 
-def snowflake(turtle, depth, lengde):
+def snowflake_side(turtle, depth, lengde):  
     if depth == 0:
-        turtle.forward(10)          # utfør F
-    else:                          # F ->
-        snowflake(turtle, depth-1, lengde/3)  #      F
-        turtle.left(60)             #        +
-        snowflake(turtle, depth-1, lengde/3)  #          F
-        turtle.right(60)            #            -
-        turtle.right(60)            #              -  
-        snowflake(turtle, depth-1, lengde/3)  #                F
-        turtle.left(60)             #                  +
-        snowflake(turtle, depth-1, lengde/3)  #                    F
-#kode hit er hentet/kopiert ihht instruks i oppgaven. 
-#Legger til en bit(lengde) kode slik at snøfnugget sålangt vises, for å være sikker på at koden stemmer før jeg begynner å legge til kode. 
+        turtle.forward(lengde)     # utfør F
+    else:
+        snowflake_side(turtle, depth-1, lengde/3)   # F ->
+        turtle.left(60)            # +
+        snowflake_side(turtle, depth-1, lengde/3)   # F
+        turtle.right(120)          # -
+        snowflake_side(turtle, depth-1, lengde/3)   # F
+        turtle.left(60)            # +
+        snowflake_side(turtle, depth-1, lengde/3)   # F
+
+def snowflake(turtle, depth, lengde):
+    for _ in range(3):
+        snowflake_side(turtle, depth, lengde)
+        turtle.right(120)
+
+# Legger til en bit(lengde)-kode slik at snøfnugget så langt vises, for å være sikker på at koden stemmer før jeg begynner å legge til flere sider.
 t = tpl.Turtle(interactive=False)
 
-snoefnugg_start_lengde = 150    # Endre til større eller mindre for å sjekke synlighet
-repetition_depth = 3            # 2 for enkel test, 3 for mer detaljert
-snowflake(t, repetition_depth, snoefnugg_start_lengde)
+repetition_depth = 3     # 2 for enkel test, 3 for mer detaljert
+snowflake(t, repetition_depth, 50)  # Endre til større eller mindre for å sjekke synlighet
 
+plt.axis('equal')
+plt.xlim(-90, 90)
+plt.ylim(-70, 70)
 plt.show()
+
